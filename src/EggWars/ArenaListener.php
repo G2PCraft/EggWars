b83a24abf7c25490a76551265296b21253930531
kaliiksPro <kaliiksPro@users.noreply.github.com>
2017-03-04 16:20:17 +0100

    Update ArenaListener.php

--- src/EggWars/ArenaListener.php
+++ src/EggWars/ArenaListener.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace EggWars;
+
+class ArenaListener implements Listener{
+  
+  public $ew;
+  
+   public function __construct(Main $ew){
+     $ew = $this->ew;
+    }
+ 
+  public function createSign(SignChangeEvent $e){
+    $p = $e->getPlayer();
+    $arena = $e->getLine(1);
+    if($p->isOp()){
+      if($e->getLine(0) == "eggwars"){
+        if(!empty($e->getLine(1)){
+          if(!$this->ew->arenaExists($e->getLine(1)){
+            $e->setLine(0, $this->ew->signprefix);
+            $e->setLine(1, "§a$arena");
+            $e->setLine(2, "§e0/0");
+            $e->setLine(3, "§7---");
+        }
+      }
+    }
+  }
+}
