9017502d4796064dc223cb4419ea6b87cbd38e56
kaliiksPro <kaliiksPro@users.noreply.github.com>
2017-03-04 15:01:36 +0100

    Update Main.php

--- src/EggWars/Main.php
+++ src/EggWars/Main.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace EggWars;
+
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    @mkdir($this->getDataFolder());
+    @mkdir($this->getDataFolder() . "Arenas");
+    @mkdir($this->getDataFolder() . "Backups");
+    self::registerEvents();
+  }
+  
+  public function registerEvents(){
+    Server::getInstance()->registerEvents(new ArenaListener($this), $this);
+    Server::getInstance()->getScheduler()->scheduleRepeatingTask(new GameTask($this), 20);
+    Server::getInstance()->getScheduler()->scheduleRepeatingTask(new SignTask($this), 20);
+  }
+}
