179953d3d80e37c50e00f6d40c7ebc494eed3d29
kaliiksPro <kaliiksPro@users.noreply.github.com>
2017-03-04 15:08:14 +0100

    Update Main.php

--- src/EggWars/Main.php
+++ src/EggWars/Main.php
@@ -16,4 +16,24 @@ class Main extends PluginBase implements Listener{
     Server::getInstance()->getScheduler()->scheduleRepeatingTask(new GameTask($this), 20);
     Server::getInstance()->getScheduler()->scheduleRepeatingTask(new SignTask($this), 20);
   }
+  
+  public function teams(){
+    $teams = array(
+    "RED" => "§c",
+    "GREEN" => "§a",
+    "YELLOW" => "§e",
+    "BLUE" => "§9"
+    );
+    return $teams;
+  }
+  
+  public function teamNumber(){
+  $tn = array(
+  "RED" => 1,
+  "GREEN" => 2,
+  "YELLOW" => 3,
+  "BLUE" => 4
+    );
+    return $tn;
+  }
 }
